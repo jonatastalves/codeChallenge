1ea37b18e93541ef36cac88c72bc471fce67ad2d
jonatastalves <jonatas.t.alves@gmail.com>
2022-04-10 17:19:34 -0300

    comentario js docs adicionado nas validacoes

--- src/services/validacoes.js
+++ src/services/validacoes.js
@@ -1,6 +1,11 @@
 import { medidasPadroes, areas, alturaMinima } from "../services/constantes.js";
 
 class Validacoes {
+  /**
+   * 
+   * @param {object} parede 
+   * @returns boolean 
+   */
   static validaAltura(parede) {
     let alturaMinimaParede = 0;
     const altura = parseInt(parede.altura);
@@ -13,7 +18,11 @@ class Validacoes {
       return false;
     }
   }
-
+/**
+ * 
+ * @param {object} parede 
+ * @returns boolean
+ */
   static temPorta(parede) {
     
     if (parede.numeroDePortas && parede.numeroDePortas > 0) {
@@ -22,7 +31,12 @@ class Validacoes {
       return false;
     }
   }
-
+/**
+ * 
+ * @param {Array<object>} body 
+ * @param {number} length 
+ * @returns {Array<object>}
+ */
   static validaBody(body, length) {
     if (!body.length == length) return [false];
     
@@ -38,7 +52,11 @@ class Validacoes {
     }
     return arrReturn;
   }
-
+/**
+ * 
+ * @param {object} objValidado 
+ * @returns boolean
+ */
   static objValido(objValidado) {
     if (!objValidado.includes(false)) {
       return true;
@@ -46,7 +64,11 @@ class Validacoes {
       return false;
     }
   }
-
+/**
+ * 
+ * @param {object} parede 
+ * @returns boolean
+ */
   static validaAreaPortasEJanelas(parede) {
     const areaParede = parede.altura * parede.largura;
     const areaPortas = parede.numeroDePortas * areas.areaPorta;
@@ -58,7 +80,11 @@ class Validacoes {
       return false;
     }
   }
-
+/**
+ * 
+ * @param {object} parede 
+ * @returns boolean
+ */
   static validaArea(parede) {
     if (!this.validaAreaPortasEJanelas(parede)) return false;
 
